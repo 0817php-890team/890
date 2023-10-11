9b17e98afe19bd6dcf917e151fa948a227ce9542
rokgt <knight_gt@naver.com>
2023-10-11 18:11:27 +0900

    1011

---
+++ 890_detail.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=tk, initial-scale=1.0">
+	<link rel="stylesheet" href="./890_detail.css">
+	<title>상세페이지</title>
+</head>
+<body>
+	
+		<h1><img class=img_cart src="./cart.png" alt="">장보자</h1>
+	
+	<main class="container">
+		<div>
+			<img class=img_dt src="./ecnc0cbcf71-706c-43bd-8744-1bc239cb2b19.jpg" alt="">
+			<div><div class="div_ylw">제목명</div><div class="div_wt"></div></div>
+			<div class="div_memo">메모aanvwvaowdfianwqnvrweyvnawyvbuaewnrgviawurvgtiaunvrgioanvgotianrvoiawunvotuawnvoiutwgnovieutangwoivtngoiwungviawvngasevawerwevrawerawerawevrwaer
+				</div>
+			<div><div class="div_ylw">수량</div><div class="div_wt"></div></div>
+			<div><div class="div_ylw">태그</div><div class="div_wt"></div></div>
+			<div><div class="div_ylw">기한</div><div class="div_wt"></div></div>
+		</div>
+		
+
+	</main>
+	<section>
+		<a href="">삭제</a>
+		<a href="">취소</a>
+	</section>
+	<footer>
+		<div><a  href=""><img class=img_add src="../img/cart_add.png" alt=""></a></div>
+		<div><a href=""><img class=img_plus src="../img/plus2.png" alt=""></a></div>
+		<div><a href="">TOP</a></div>
+	</footer>
+</body>
+</html>
--- footer.php
+++ footer.php
@@ -1,5 +1,17 @@
 <footer>
-    <div>완료된</div>
-    <div>작성페이지</div>
-    <div>TOP</div>
+    <div>
+        <a  href="">
+            <img class=img_add src="../img/cart_add.png" alt="">
+        </a>
+    </div>
+    <div>
+        <a href="">
+            <img class=img_plus src="../img/plus2.png" alt="">
+        </a>
+    </div>
+    <div>
+        <a href="">
+            TOP
+        </a>
+    </div>
 </footer>
