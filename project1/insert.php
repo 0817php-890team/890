81eb44e346c7dc5f89b1667b78448e688f74a8db
tdc00161 <tdc00161@naver.com>
2023-10-12 18:05:03 +0900

    20231012 update

--- project1/insert.php
+++ project1/insert.php
@@ -2,70 +2,51 @@
 define( "ROOT", $_SERVER["DOCUMENT_ROOT"]."/project1");
 define( "FILE_HEADER", ROOT ."/header.php" );
 define( "FILE_FOOTER", ROOT ."/footer.php" );
+define( "ERROR_MSG_PARAM", "오류입니다~" );
 require_once( ROOT ."/lib_db.php" );
 
 
 // var_dump();
 
-$title="";
-$content="";
 
-// $http_method = $_SERVER["REQUEST_METHOD"];
-// if($http_method === "POST") {
-// 	try {
-// 		$arr_post = $_POST;
-// 		$conn = null; // DB Connection 변수
+$http_method = $_SERVER["REQUEST_METHOD"];
+if($http_method === "POST") {
+		
+		$conn = null; // DB Connection 변수
 
-// 		//파라미터 획득
-// 		$item_name = isset($_POST["item_name"]) ? trim($_POST["item_name"]) : ""; // item_name 셋팅
-// 		$tag_id = isset($_POST["tag_id"]) ? trim($_POST["tag_id"]) : ""; // tag_id 셋팅
-// 		$finished_at = isset($_POST["finished_at"]) ? trim($_POST["finished_at"]) : ""; // finished_at 셋팅
-// 		$img = isset($_POST["img"]) ? trim($_POST["img"]) : ""; // img 셋팅
+		//파라미터 획득
+		$item_name = $_POST["item_name"];
+		//$item_name = isset($_POST["item_name"]) ? trim($_POST["item_name"]) : ""; // item_name 셋팅
+		$tag_id = isset($_POST["tag_id"]) ? trim($_POST["tag_id"]) : ""; // tag_id 셋팅
+		$d_day = isset($_POST["d_day"]) ? trim($_POST["d_day"]) : ""; // finished_at 셋팅
+		$img = isset($_POST["img"]) ? trim($_POST["img"]) : ""; // img 셋팅
+		$memo = isset($_POST["memo"]) ? trim($_POST["memo"]) : ""; // img 셋팅
+		$amount = isset($_POST["amount"]) ? trim($_POST["amount"]) : ""; // img 셋팅
+		var_dump($item_name);
+			PDO_set($conn);
+			$conn->beginTransaction(); // 트랜잭션 시작
 
-// 		if($title === "") {
-// 			$arr_err_msg[] = sprintf(ERROR_MSG_PARAM, "제목");
-// 		}
-// 		if($title === "") {
-// 			$arr_err_msg[] = sprintf(ERROR_MSG_PARAM, "내용");
-// 		}
+			// 글 작성을 위해 파라미터 셋팅
+			$arr_param = [
+				"item_name" => $item_name
+				,"memo" => $memo
+				,"tag_id" => $tag_id
+				,"d_day" => $d_day
+				,"img" => $img
+				,"amount" => $amount
+			];
 
-// 		if(count($arr_err_msg) === 0) {
-// 			// DB 접속
-// 			if(!my_db_conn($conn)) {
-// 				// DB Instance 에러
-// 				throw new Exception("DB Error : PDO Instance");
-// 			}
-// 			$conn->beginTransaction(); // 트랜잭션 시작
+			// insert
+			// db_insert_boards($conn, $arr_param);
 
-// 			// 글 작성을 위해 파라미터 셋팅
-// 			$arr_param = [
-// 				"item_name" => $_POST["item_name"]
-// 				,"tag_id" => $_POST["tag_id"]
-// 				,"finished_at" => $_POST["finished_at"]
-// 				,"img" => $_POST["img"]
-// 			];
+			$conn->commit(); //모든 처리 완료 시 커밋
 
-// 			// insert
-// 			if(db_insert_boards($conn, $arr_post)) {
-// 				// DB Insert 에러
-// 				throw new Exception("DB Error : Insert Boards");
-// 			}
-			
-// 			$conn->commit(); //모든 처리 완료 시 커밋
+			// 리스트 페이지로 이동
+			// header("Location: list.php");
+			// exit;
 
-// 			// 리스트 페이지로 이동
-// 			header("Location: list.php");
-// 			exit;
-// 		}
-// 	} catch(Exception $e) {
-// 		// echo $e->getMessage(); // Exception 메세지 출력
-// 		header("Location: error.php/?err_msg={$e->getMessage()}");
-// 		exit;
-// 	} finally {
-// 		db_destroy_conn($conn); // DB 파기
-// 	}
-// }
 
+		}
 
 
 ?>
@@ -87,7 +68,7 @@ $content="";
 			<div class="insert_ma">
 					<div class="insert_div">
 					<div class="insert_tit">제품명</div>
-					<input class="insert_in2" type="text" maxlength="50" required>
+					<input class="insert_in2" type="text" name="item_name" maxlength="50" required>
 					<div class="insert_te"></div>
 				</div>
 				<textarea class="insert_me" name="memo" id="memo" cols="25" rows="30" placeholder="메모"></textarea>
@@ -98,7 +79,7 @@ $content="";
 				</div>
 				<div class="insert_div">
 					<div class="insert_tit">태그</div>
-					<select class="insert_in2" name="tag" id="tag">
+					<select class="insert_in2" name="tag_id" id="tag">
 						<option value="" selected disabled hidden>선택해주세요</option>
 						<option value="clothes">패션/뷰티</option>
 						<option value="food">식품/생활</option>
